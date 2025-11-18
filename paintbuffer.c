d3edf47553d6aad4d54661fc51613879403f1087
jsjlim27 <jsjlim27@gmail.com>
2025-11-18 02:06:25 -0800

    cleaned up comments

--- paintbuffer.c
+++ paintbuffer.c
@@ -1,9 +1,7 @@
 #include "drawpad.h"
 
 #define BUF_SIZE 4096 
-// #define INTERP_SIZE 2048
 
-// static SDL_Rect Specks[BUF_SIZE * (1 + INTERP_SIZE)];
 static SDL_Rect Specks[BUF_SIZE];
 
 static SDL_Rect *rear = Specks; 
@@ -27,24 +25,14 @@ void retrieve_paint(struct DrawPadStates *AppStates)
 {
         if (front > rear)
         {
-                // *data = rear;
 		AppStates->data = rear;
-
-                // *n_data = front - rear;
 		AppStates->dataLength = front - rear;
         }
         else if (front < rear)
         {
-                // *data = Specks;
 		AppStates->data = Specks;
-
-                // *n_data = front - Specks;
 		AppStates->dataLength = front - Specks;
-
-                // *data_extra = rear;
 		AppStates->aux = rear;
-
-                // *n_data_extra = Specks + BUF_SIZE - rear;
 		AppStates->auxLength = Specks + BUF_SIZE - rear;
         }
 	else // (front == rear)
@@ -57,9 +45,3 @@ void retrieve_paint(struct DrawPadStates *AppStates)
 
         rear = front;
 }
-
-// how to implement undo feature? 
-// just "rewind" the Specks to previous "stroke"
-// as you traverse backwards the stroke region of the Specks,
-// set the alpha value of each Rect element to zero!
-// this approach might be buggy when there are Rect objects under the same coordinates.
